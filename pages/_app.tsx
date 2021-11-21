b5e5b558199956210469012573b95e1ac05cc46a
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-21 18:39:47 +0100

    refactor: MyApp -> App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@components/Layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
@@ -10,4 +10,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+export default App
