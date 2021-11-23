d3e37b646f2dcfc9618ae012586fd679555645bc
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-23 22:56:08 +0100

    chore: remove docs references

--- pages/api/visits/[id].ts
+++ pages/api/visits/[id].ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import _ from 'lodash'
 import { IVisit, Visit, VisitKeys } from '@database/models/visit'
