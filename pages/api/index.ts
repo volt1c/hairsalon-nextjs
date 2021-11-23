9893288b76171f7d38b8f5b919b9ee17f1267a09
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-23 22:36:50 +0100

    feat: api init

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  plannedVisits: any
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).end('Welcom to API!')
 }
