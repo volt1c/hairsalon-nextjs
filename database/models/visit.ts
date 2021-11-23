f53b56fe42584f9122ff183265de9fc55c79fe4c
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-23 22:34:56 +0100

    feat: add visit shema and interfaces

---
+++ database/models/visit.ts
@@ -0,0 +1,30 @@
+import { Schema, Document, model, models } from 'mongoose'
+
+const VisitKeys = ['_id', 'name', 'surename', 'email', 'phone', 'date']
+const visitShema = new Schema({
+  name: { type: String, required: true },
+  surename: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  date: { type: String, required: true },
+})
+
+interface IVisit extends Document {
+  _id: string
+  name: string
+  surename: string
+  email: string
+  phone: string
+  date: string
+}
+interface ICreateVisit {
+  name: IVisit['name']
+  surename: IVisit['surename']
+  email: IVisit['email']
+  phone: IVisit['phone']
+  date: IVisit['date']
+}
+const Visit = models.Visit || model<IVisit>('Visit', visitShema)
+
+export type { IVisit, ICreateVisit }
+export { Visit, VisitKeys }
