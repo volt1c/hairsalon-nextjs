a86dca68400d8746ec5b3a8576ee9c0b984d78cb
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-23 22:49:05 +0100

    refactor: remove unused `type Data ...`

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,13 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  plannedVisits: any
-}
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<string>
 ) {
   res.status(200).end('Welcom to API!')
 }
