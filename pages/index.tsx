489bd7dc937886c78f60d59b6bf2dec30b647485
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-23 22:44:22 +0100

    refactor: remove `Layout` from _app.tsx layout is still set on pages !

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
 import styles from '@styles/Home.module.css'
+import Layout from '@layouts/default'
 
 const IndexPage: NextPage = () => {
   return (
-    <div className={styles.main}>
-      <h1 className={styles.banner}>The best hair salon in the city</h1>
-      <p className={styles.p}>We cut your hair and give them new life.</p>
-      <Link href="/book">
-        <a className={styles.buttonLink}>Book now</a>
-      </Link>
-    </div>
+    <Layout>
+      <div className={styles.main}>
+        <h1 className={styles.banner}>The best hair salon in the city</h1>
+        <p className={styles.p}>We cut your hair and give them new life.</p>
+        <Link href="/book">
+          <a className={styles.buttonLink}>Book now</a>
+        </Link>
+      </div>
+    </Layout>
   )
 }
 
