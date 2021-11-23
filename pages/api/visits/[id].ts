19df36d2d820c4f1ac64a27731687e2f2edd8898
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-23 22:54:13 +0100

    refactor: remove redundancies, actions reordering

--- pages/api/visits/[id].ts
+++ pages/api/visits/[id].ts
@@ -5,26 +5,24 @@ import { IVisit, Visit, VisitKeys } from '@database/models/visit'
 import dbConnect from '@database/dbConnect'
 
 dbConnect()
-type Data = IVisit
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<IVisit>
 ) {
   const {
     query: { id },
     method,
   } = req
 
-  if (method != 'GET') {
-    res.setHeader('Allow', ['GET'])
-    res.status(405).end(`Method ${method} Not Allowed`)
+  if (method == 'GET') {
+    const visit: IVisit = _.pick(
+      await Visit.findOne({ _id: id }).exec(),
+      VisitKeys
+    ) as IVisit
+    res.status(200).json(visit)
   }
 
-  const visit: IVisit = _.pick(
-    await Visit.findOne({ _id: id }).exec(),
-    VisitKeys
-  ) as IVisit
-
-  res.status(200).json(visit)
+  res.setHeader('Allow', ['GET', 'POST', 'DELETE'])
+  res.status(405).end(`Method ${method} Not Allowed`)
 }
