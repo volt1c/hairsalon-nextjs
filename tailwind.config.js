8d61351576f348193c8cb745094919a74498e20b
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-25 18:13:28 +0100

    chore: add tailwind, VechaiUI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./node_modules/@vechaiui/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@vechaiui/core')],
+}
