4b026557050f669a9bc2e321efc8d0cabfa28cde
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-26 18:16:30 +0100

    feat: empty components Footer, InfoBox

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,14 +1,9 @@
 import { ReactElement } from 'react'
-import styles from '@styles/components/InfoBox.module.css'
 
 type InfoBoxProps = {
   children: ReactElement | ReactElement[]
 }
 
-const InfoBox = ({ children }: InfoBoxProps): ReactElement => (
-  <div className={styles.self}>
-    <div className={styles.box}>{children}</div>
-  </div>
-)
+const InfoBox = ({ children }: InfoBoxProps): ReactElement => <div></div>
 
 export default InfoBox
