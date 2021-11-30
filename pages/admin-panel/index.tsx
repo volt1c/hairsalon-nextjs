467131e2e8a9e86759b924f3321d4202d3718990
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-11-30 22:44:29 +0100

    feat: init `pages/admin-panel/`

--- pages/admin-panel/index.tsx
+++ pages/admin-panel/index.tsx
@@ -1,7 +1,11 @@
+import AdminPanelLayout from '@layouts/AdminPanelLayout'
 import { NextPage } from 'next'
+import React from 'react'
 
-const IndexPage: NextPage = () => {
-  return <></>
-}
+const IndexPage: NextPage = () => (
+  <AdminPanelLayout>
+    <h1>test</h1>
+  </AdminPanelLayout>
+)
 
 export default IndexPage
