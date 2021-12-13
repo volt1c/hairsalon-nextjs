426641658231f2dcb3dd4eb3cf8b5cbf8427bf52
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-12-13 10:58:09 +0100

    feat: new methods on `api/permission` new: 'GET', 'POST'

--- pages/api/permission/index.ts
+++ pages/api/permission/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '@database/dbConnect'
 import _ from 'lodash'
-import { Admin, IAdmin } from '@database/models/admin'
+import { Admin, IAdmin, ICreateAdmin } from '@database/models/admin'
 import { getSession } from 'next-auth/react'
 import hasPermission from '@utils/hasPermission'
 
@@ -13,15 +13,40 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const session = await getSession()
-  const { method } = req
+  const { method, body } = req
 
   switch (method) {
+    case 'GET':
+      if (await hasPermission(session?.user)) {
+        const admins = await Admin.find({}).exec()
+        res.status(200).json(admins)
+      } else {
+        res.status(401).end()
+      }
+      return
+
+    case 'POST':
+      if (await hasPermission(session?.user)) {
+        //todo proper data validation
+        try {
+          const admin = await Admin.create({
+            email: body.email,
+          })
+          res.status(201).end()
+        } catch (err) {
+          res.status(422).end()
+        }
+      } else {
+        res.status(401).end()
+      }
+      return
+
     case 'HEAD':
       res.status((await hasPermission(session?.user)) ? 200 : 401).end()
       return
 
     default:
-      res.setHeader('Allow', ['HEAD'])
+      res.setHeader('Allow', ['GET', 'POST', 'HEAD'])
       res.status(405).end(`Method ${method} Not Allowed`)
       return
   }
