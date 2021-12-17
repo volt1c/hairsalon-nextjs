01a7267ce4eeea4c984428062f0e2d4ffa8490fa
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-12-17 21:09:58 +0100

    feat: improve page `/book`

--- pages/book.tsx
+++ pages/book.tsx
@@ -24,37 +24,47 @@ function Book({
         <title>Hairsalon - Book an appointment</title>
       </Head>
 
-      <InfoBox className="flex flex-row flex-initial">
-        <div className=" flex-grow-0 bg-neutral-300 bg-opacity-20 p-5">
+      <InfoBox className="p-0 border-primary-400 flex flex-row flex-initial mx-24 my-auto">
+        <div className=" flex-grow-0 bg-neutral-300 bg-opacity-20 p-5 w-64">
           <div className="mb-3">
             <h1 className="text-2xl pb-4">Schedule: </h1>
 
-            <h2 className="text-lg pb-4">When we are open:</h2>
-            <p>Work days:</p>
-            <ul>
-              {schedule.workWeekDays.map((day) => (
-                <li key={day.toString()} className="ml-2">{`${day}`}</li>
-              ))}
-            </ul>
-            <p>Open hours:</p>
-            <ul>
-              {schedule.openHours.map((hour) => (
-                <li key={hour.toString()} className="ml-2">{`${hour}:00`}</li>
-              ))}
-            </ul>
-            <p>Planning scope:</p>
-            <ul>
-              <li key={schedule.planningScope.toString()} className="ml-2">
-                {schedule.planningScope} days
-              </li>
-            </ul>
+            <h2 className="text-lg pb-2">When we are open:</h2>
+            <div className="pl-3">
+              <p>Work days:</p>
+              <ul>
+                {schedule.workWeekDays.map((day) => (
+                  <li key={day.toString()} className="ml-2">
+                    - {`${day}`}
+                  </li>
+                ))}
+              </ul>
+              <p>Open hours:</p>
+              <ul>
+                {schedule.openHours.map((hour) => (
+                  <li key={hour.toString()} className="ml-2">
+                    - {`${hour}:00`}
+                  </li>
+                ))}
+              </ul>
+              <p>Planning scope:</p>
+              <ul>
+                <li key={schedule.planningScope.toString()} className="ml-2">
+                  - {schedule.planningScope} days
+                </li>
+              </ul>
+            </div>
           </div>
           <ul></ul>
         </div>
 
         <div className="flex flex-wrap flex-col w-full p-5">
           <h1 className="p-4 text-3xl ">Book an appointment</h1>
-          <FormAddVisit avaliable={avaliable} notification={notification} />
+          <FormAddVisit
+            redirectUrl="/"
+            avaliable={avaliable}
+            notification={notification}
+          />
         </div>
       </InfoBox>
     </MainLayout>
